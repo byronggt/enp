9e292b96f69c17bc76469d11cff13c794400b4b9
ByronG <byrong.gt@gmail.com>
2023-07-03 11:32:06 -0600

    Actualizada la prueba de Shapiro & Wilk

--- análisis de frecuencias II/prueba_shapiro_wilk.R
+++ análisis de frecuencias II/prueba_shapiro_wilk.R
@@ -4,3 +4,9 @@
 
 if(!require(car)){install.packages("car")}
 if(!require(tidyverse)){install.packages("tidyverse")}
+if(!require(readxl)){install.packages("readxl")}
+
+## Importar la tabla de datos "estudiantes_medidas_cardiov"
+estud<-read_excel("estudiantes_medidas_cardiov.xlsx")
+
+
