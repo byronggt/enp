267cdcaa59e4b926faff53c395276a92b48e412e
ByronG <byrong.gt@gmail.com>
2023-07-03 11:45:38 -0600

    Actualizada la prueba de Shapiro & Wilk

--- análisis de frecuencias II/prueba_shapiro_wilk.R
+++ análisis de frecuencias II/prueba_shapiro_wilk.R
@@ -9,4 +9,25 @@ if(!require(readxl)){install.packages("readxl")}
 ## Importar la tabla de datos "estudiantes_medidas_cardiov"
 estud<-read_excel("estudiantes_medidas_cardiov.xlsx")
 
+# Separar los registros para hombres y mujeres
+estud_h<-estud %>% 
+  select (sexo,talla) %>% 
+  filter(sexo=="M")
+
+estud_m<-estud %>% 
+  select (sexo,talla) %>% 
+  filter(sexo=="F")
+
+# Solicitar QQ Plot para talla de hombres
+qqPlot(estud_h$talla)
+
+
+# Solicitar QQ Plot para talla de mujeres
+qqPlot(estud_m$talla)
+
+
+# Calcular el estadístico de prueba de Shapiro & Wilk
+shapiro.test(estud_h$talla)
+
+shapiro.test(estud_m$talla)
 
