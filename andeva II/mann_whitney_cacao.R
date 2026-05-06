4a6c7b2b59a99148af8be127ecfc22e9f1c6977a
byronggt <byrong.gt@gmail.com>
2026-05-06 10:02:09 -0600

    Actualizado Mann-Whitney

--- andeva II/mann_whitney_cacao.R
+++ andeva II/mann_whitney_cacao.R
@@ -1,6 +1,5 @@
 # Dr. Byron González
-# Dr. Ezequiel López
-# http://cete.fausac.gt
+# http://byrong.cc 
 
 if(!require(coin)){install.packages("coin")}
 if(!require(readxl)){install.packages("readxl")}
